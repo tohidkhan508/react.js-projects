1282015b87d08ec32102765c83b31f014cf9c04e
tohidkhann508 <tohidkhann508@gmail.com>
2026-06-20 05:37:25 +0530

    porject 6

--- react-project-06/src/main.jsx
+++ react-project-06/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme/index";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
-)
+);
